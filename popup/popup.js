d0e8bba31c6efd1957654c2f894e167f32c9dc28
idansaban <idan23s.g@gmail.com>
2020-03-28 16:23:29 +0300

    close popup + enter click listener

--- popup/popup.js
+++ popup/popup.js
@@ -1,20 +1,26 @@
+const setTitle = (setButton, resetButton, titleInput) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        let { value } = titleInput;
+        let activeTab = tabs[0];
+        chrome.runtime.sendMessage({ message: "set_title", activeTab, value });
+    });
+    window.close();
+};
+
 document.addEventListener('DOMContentLoaded', () => {
     let setButton = document.getElementById('setButton');
     let resetButton = document.getElementById('resetButton');
     let titleInput = document.getElementById('titleInput');
 
-    setButton.addEventListener('click', () => {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            let { value } = titleInput;
-            let activeTab = tabs[0];
-            chrome.runtime.sendMessage({ message: "set_title", activeTab, value });
-        });
-    });
+    setButton.addEventListener('click', () => setTitle(setButton, resetButton, titleInput));
+
+    titleInput.addEventListener('keyup', e => { if (e.keyCode == 13) setTitle(setButton, resetButton, titleInput); });
 
     resetButton.addEventListener('click', () => {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             let activeTab = tabs[0];
             chrome.runtime.sendMessage({ message: "reset_title", activeTab });
         });
+        window.close();
     });
 });
